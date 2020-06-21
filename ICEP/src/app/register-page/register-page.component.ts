6bf8e5082a90bd7a9e8a18739fe7b7d9fda71d31
Moora <43159712+MooraBlack@users.noreply.github.com>
2020-06-21 10:13:25 +0200

    Gallery header main register

--- ICEP/src/app/register-page/register-page.component.ts
+++ ICEP/src/app/register-page/register-page.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
+
+
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
