64a1a7784cf7077490ffe46ad14dfb841df9a2fd
Moora <43159712+MooraBlack@users.noreply.github.com>
2020-07-17 01:23:24 +0200

    Ashoka

--- ICEP/src/app/app.module.ts
+++ ICEP/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+<<<<<<< HEAD
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule} from '@angular/forms';
 
 
+=======
+import { Routes, RouterModule } from '@angular/router'; // leave this alone
+import { FormsModule} from '@angular/forms';
+
+>>>>>>> parent of 67b9e88... Revert "Ashoka"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,10 +32,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { AdminLoginPageComponent } from './admin-login-page/admin-login-page.component';
 import { EmailFeedbackComponent } from './email-feedback/email-feedback.component';
+<<<<<<< HEAD
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './_component/alert/alert.component';
 
 
+=======
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+>>>>>>> parent of 67b9e88... Revert "Ashoka"
 
 @NgModule({
   declarations: [
@@ -61,7 +72,12 @@ import { AlertComponent } from './_component/alert/alert.component';
     FormsModule,
     AppRoutingModule,
     NgbModule,
+<<<<<<< HEAD
     ReactiveFormsModule
+=======
+    BrowserAnimationsModule,
+    MaterialModule,
+>>>>>>> parent of 67b9e88... Revert "Ashoka"
   ],
   providers: [],
   bootstrap: [AppComponent]
